3b5281bb469bdcde5ec51eea0b62610fb624ed8c
Luo Ming Xuan <66114685+luomingxuan@users.noreply.github.com>
2024-08-15 17:21:10 +0800

    Add files via upload

---
+++ pretrain_models/Bert/bert_modules/bert.py
@@ -0,0 +1,43 @@
+from torch import nn as nn
+
+from bert_modules.embedding import BERTEmbedding
+from bert_modules.transformer import TransformerBlock
+
+class BERT(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+
+
+        # self.init_weights()
+
+        max_len = args.bert_max_len
+        num_items = args.num_items
+        n_layers = args.bert_num_blocks
+        heads = args.bert_num_heads
+        #这里是因为我的id从1开始算，且仍然需要一个mask token id，所以+2
+        vocab_size = num_items + 2
+        hidden = args.bert_hidden_units
+        self.hidden = hidden
+        dropout = args.bert_dropout
+
+        # embedding for BERT, sum of positional, segment, token embeddings
+        self.embedding = BERTEmbedding(vocab_size=vocab_size, embed_size=self.hidden, max_len=max_len, dropout=dropout)
+
+        # multi-layers transformer blocks, deep network
+        self.transformer_blocks = nn.ModuleList(
+            [TransformerBlock(hidden, heads, hidden * 4, dropout) for _ in range(n_layers)])
+
+    def forward(self, x):
+        mask = (x > 0).unsqueeze(1).repeat(1, x.size(1), 1).unsqueeze(1)
+
+        # embedding the indexed sequence to sequence of vectors
+        x = self.embedding(x)
+
+        # running over multiple transformer blocks
+        for transformer in self.transformer_blocks:
+            x = transformer.forward(x, mask)
+
+        return x
+
+    def init_weights(self):
+        pass
